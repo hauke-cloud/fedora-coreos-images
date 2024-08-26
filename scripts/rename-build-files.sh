9fb7d19734cdaef0a2f93c878407a96a5f286a49
Hauke Mettendorf <hauke@mettendorf.it>
2024-08-26 15:29:21 +0200

    Added first version container build

---
+++ scripts/rename-build-files.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+OUTPUT_DIRECTORY="output"
+
+# Get first build directory
+BUILD_NAME=$(find . -maxdepth 1 -type d ! -name '.*' | xargs -I {} basename {})
+
+# Searching for valid build directory
+BUILD_DIRECTORY=""
+for directory in ${BUILD_NAME}; do
+  if [[ "${directory}" =~ ^[0-9]{2}\.[0-9]{8}\.[a-z]{3,4}\.[0-9]{1}$ ]]; then
+    BUILD_DIRECTORY="${directory}"
+  fi
+done
+
+# Check if build directory is valid
+if [[ -z "${BUILD_DIRECTORY}" ]]; then
+  echo "No valid build directory found!"
+  exit 1
+else
+  echo "Build directory found: ${BUILD_DIRECTORY}"
+fi
+
+# Create output directory
+if [[ ! -d "${OUTPUT_DIRECTORY}" ]]; then
+  echo "Creating output directory ${OUTPUT_DIRECTORY}"
+  mkdir -p ${OUTPUT_DIRECTORY}
+fi
+
+# Process files
+for file in $(find ${BUILD_DIRECTORY} -type f); do
+  # Get file name without extension
+  FILE_NAME=$(basename "${file}" .${FILE_EXTENSION})
+
+  # Get file extension
+  FILE_EXTENSION="${file##*.}"
+
+  # Get file directory
+  FILE_DIRECTORY=$(dirname "${file}")
+
+  # Get architecture
+  ARCHITECTURE=$(echo ${FILE_DIRECTORY} | cut -d/ -f 2)
+
+  # Skip unnecessary files
+  if [[ "${FILE_EXTENSION}" =~ ^(raw|json|ociarchive)$ ]]; then
+    echo "Skipping unnecessary file ${FILE_NAME}"
+    continue
+  fi
+
+  # Create architecture directory
+  if [[ ! -d "${OUTPUT_DIRECTORY}/${ARCHITECTURE}" ]]; then
+    echo "Creating architecture directory ${OUTPUT_DIRECTORY}/${ARCHITECTURE}"
+    mkdir -p ${OUTPUT_DIRECTORY}/${ARCHITECTURE}
+  fi
+
+  if [[ "${FILE_NAME}" =~ fedora-coreos- ]]; then
+    echo "Copying ${FILE_NAME} to ${OUTPUT_DIRECTORY}/${ARCHITECTURE}/${FILE_NAME}"
+    NEW_FILE_NAME=$(echo ${FILE_NAME} | sed -E "s/fedora-coreos-${BUILD_DIRECTORY}/latest/g")
+    mv ${file} ${OUTPUT_DIRECTORY}/${ARCHITECTURE}/${NEW_FILE_NAME}
+  fi
+done
