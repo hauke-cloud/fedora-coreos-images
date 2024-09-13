971f359925a976a9d84da2a0c5043ccef3c3432d
Hauke Mettendorf <hauke@mettendorf.email>
2024-09-13 21:51:09 +0200

    Optimized file names

--- scripts/rename-build-files.sh
+++ scripts/rename-build-files.sh
@@ -54,7 +54,8 @@ for file in $(find ${BUILD_DIRECTORY} -type f); do
   fi
 
   if [[ "${FILE_NAME}" =~ fedora-coreos- ]]; then
-    NEW_FILE_NAME=$(echo ${FILE_NAME} | sed -E "s/fedora-coreos-${BUILD_DIRECTORY}-//g" | sed -E "s/-${ARCHITECTURE}//g")
+    NEW_FILE_BASE=$(echo ${FILE_NAME} | sed -E "s/fedora-coreos-${BUILD_DIRECTORY}//g" | cut -d"." -f 1)
+    NEW_FILE_NAME="${NEW_FILE_BASE}.${FILE_EXTENSION}"
     echo "Copying ${file} to ${OUTPUT_DIRECTORY}/${ARCHITECTURE}/${NEW_FILE_NAME}"
     mv ${file} ${OUTPUT_DIRECTORY}/${ARCHITECTURE}/${NEW_FILE_NAME}
   fi
